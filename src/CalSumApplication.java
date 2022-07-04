37e63874101a44af3859b87b851c06b52930996d
sonanh2001 <ANHNHS.B19CN026@stu.ptit.edu.vn>
2022-07-04 22:27:40 +0700

    Second Fixed

--- src/CalSumApplication.java
+++ src/CalSumApplication.java
@@ -1,13 +1,18 @@
 public class CalSumApplication {
-    public static int calSum(int start,int end){
+    public static int calSum(int num1,int num2){
         int sum=0;
-        for(int i=start;i<=end;i++){
+        if(num1>num2){
+            int temp=num1;
+            num2=num1;
+            num1=temp;
+        }
+        for(int i=num1;i<=num2;i++){
             sum+=i;
         }
         return sum;
     }
     public static void main(String[] args) {
-        int result=calSum(1,50);
+        int result=calSum(50,1);
         System.out.println(result);
     }
 }
